15d4a6cc8136d0d35e893b464d4e4794cf463429
Vo Hieu Thang <hieuthang369@gmial.com>
2021-12-19 17:32:04 +0700

    UI DONE

---
+++ src/script/main.js
@@ -0,0 +1,24 @@
+$(function () {
+    (function () {
+        const videos = [
+            {
+                avatarURL: 'https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/3a51e5fbffdd906e0085e0671a111480.jpeg?x-expires=1639983600&x-signature=FV2%2BLBEyjGuf9cghbWoJv%2FkmYjI%3D',
+                userName: 'sweetfoodr',
+                nickname: 'Sweet Food',
+                titleName: 'Sweet Food ',
+                videoURL: 'https://v16-webapp.tiktok.com/381e4740e95e87c19eb6cab701aa9a25/61bf3600/video/tos/alisg/tos-alisg-pve-0037/7048cba9c51743b8b16362e647c524b1/?a=1988&br=3736&bt=1868&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_Fpckag3-I&l=20211219073858010245244167078C8049&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anZvZjQ6ZnA1OTMzODgzNEApOWc3aDZpNzw2NzY5Ozw7OWcxMC9gcjRnMGxgLS1kLy1zcy1jYDUtXy9iXy0vNDNhYGM6Yw%3D%3D&vl=&vr=',
+                title: 'Góc đồ ăn',
+                hastag: ['food', 'cake', 'cooking', 'sweetfood', 'hastag5'],
+                musicName: 'Mood - Remix - Zero Two',
+                interacts: { heart: 342000, comment: 5344, share: 4232 }
+            },
+        ]
+        return {
+            run() {
+                console.log('Start');
+            }
+        }
+    })().run();
+})
+
+
